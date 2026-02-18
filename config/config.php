4c97c1b11229bf810700fc84396cf53cd0207be1
LaryllParaiso <larylljanparaiso.neust@gmail.com>
2026-02-18 22:05:24 +0800

    Add config/config.php

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+const APP_BASE_URL = '/BNH_ATS';
+
+const APP_TIMEZONE = 'Asia/Manila';
+
+const DB_HOST = '127.0.0.1';
+const DB_NAME = 'attendance_system';
+const DB_USER = 'root';
+const DB_PASS = '';
+
+const AUTH_SESSION_TIMEOUT_SECONDS = 1800;
+const AUTH_MAX_FAILED_LOGINS = 5;
+const AUTH_LOCKOUT_MINUTES = 15;
+
+const ATTENDANCE_GRACE_MINUTES = 15;
